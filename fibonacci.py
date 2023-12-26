2772e4d5675744764fadcb30074188df55122dcf
adautosouzaC <dautosouza1005@gmail.com>
2023-12-26 13:20:26 -0300

    lançar dados

--- fibonacci.py
+++ fibonacci.py
@@ -5,28 +5,30 @@ class Fibonacci:
         self.elemento_atual = 0
         self.proximo_elemento = 1
         self.maximo = maximo
-
+        self.contagem = 0
     def __iter__(self):
         return self
     
     def __next__(self):
-        if self.elemento_atual > self.maximo:
+        if self.contagem >= self.maximo:
             raise StopIteration
         
         valor_retorno = self.elemento_atual 
 
         self.elemento_atual = self.proximo_elemento
-        self.proximo_elementeo = valor_retorno + self.proximo_elemento
-
+        self.proximo_elemento = valor_retorno + self.proximo_elemento
+        self.contagem += 1
         return valor_retorno
     
     def __str__(self):
         return f'Fibonacci de {self.maximo}'
     
-obj_Fibonacci = Fibonacci(10)
-print(obj_Fibonacci)
-for numero in obj_Fibonacci:
-    print(numero)
+obj_Fibonacci = Fibonacci(20)
+for i in range(25):
+    try:
+        print(obj_Fibonacci.__next__())
+    except StopIteration:
+        print("Excedeu o limite")
     
     
 
